c4df34297e3844af823af729c84cf20ff4646257
Federico Versari <versari@MBP-di-Federico.station>
2023-08-05 15:25:24 +0200

    removed docopt for argparse

--- src/main.py
+++ src/main.py
@@ -1,24 +1,18 @@
-from docopt import docopt
-from Config import Config
+import argparse
 from Builder import RepertoireBuilder
-
-doc = """Usage:
-    main.py <config_file>
-
-Arguments:
-    <config_file>   Path to YAML configuration file
-"""
-
+from Config import Config
 
 def main():
-    args = docopt(doc)
-    filename = args['<config_file>']
+    parser = argparse.ArgumentParser(description="Generate repertoire using YAML configuration file.")
+    parser.add_argument('config_file', help="Path to YAML configuration file")
+    args = parser.parse_args()
+    print(f"\nARGS: {args}")
+    filename = args.config_file
 
     config = Config(filename)
 
     builder = RepertoireBuilder(config)
     builder.GenerateReportoire()
-
 
 if __name__ == '__main__':
     main()
